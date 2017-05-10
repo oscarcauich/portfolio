820c49c9ee60fea0175350946cadd871f37e57a7
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-10 08:42:03 -0700

    hamburger menu half done, when clicked the navigation displays

--- portfolioscript.js
+++ portfolioscript.js
@@ -1,2 +1,18 @@
 'use strict';
-console.log('it works');
+
+// create constructor for projects
+function PortfolioCreate(portolioObjs) {
+  this.title = portolioObjs.title;
+  this.dateCreated = portolioObjs.dateCreated;
+  this.gitHubURL = portolioObjs.gitHubURL;
+  this.portfolioPicture = portolioObjs.portfolioPicture;
+  this. smallDescription = portolioObjs.smallDescription;
+  this.fullDescrition = portolioObjs.fullDescrition;
+}
+
+$(document).ready(function() {
+  $('.lnr-menu').on('click', function(){
+    $('#main-nav').removeClass();
+    $('#main-nav').addClass('show-menu');
+  })
+});
