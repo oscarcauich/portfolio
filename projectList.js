419b87c831c055db726ddeae9e5ddf9ced9243ac
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-10 23:29:01 -0700

    project template and project array created

---
+++ projectList.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var projects = [
+  {
+    title: 'Bus Mall',
+    dateCreated: '04-20-2017',
+    portfolioPicture: 'img/portfolioPictureOne.jpeg',
+    smallDescription: 'a small description will go here',
+    fullDescrition: 'a full description will go here',
+  },
+  {
+    title: 'Salmon Cookie',
+    dateCreated: '04-13-2017',
+    portfolioPicture: 'img/portfolioPictureTwo.jpeg',
+    smallDescription: 'a small description will go here',
+    fullDescrition: 'a full description will go here',
+  },
+  {
+    title: 'About Me Game',
+    dateCreated: '04-05-2017',
+    portfolioPicture: 'img/portfolioPictureOne.jpeg',
+    smallDescription: 'a small description will go here',
+    fullDescrition: 'a full description will go here',
+  },
+];
