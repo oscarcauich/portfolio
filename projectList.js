0ca70c1cf8829f6d472e7e7416308fb545bd535b
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-11 00:21:19 -0700

    I was able to clone my projects template and rendered my projects inside my array

--- projectList.js
+++ projectList.js
@@ -3,21 +3,21 @@
 var projects = [
   {
     title: 'Bus Mall',
-    dateCreated: '04-20-2017',
+    dateCreated: '2017-04-20',
     portfolioPicture: 'img/portfolioPictureOne.jpeg',
     smallDescription: 'a small description will go here',
     fullDescrition: 'a full description will go here',
   },
   {
     title: 'Salmon Cookie',
-    dateCreated: '04-13-2017',
+    dateCreated: '2017-04-17',
     portfolioPicture: 'img/portfolioPictureTwo.jpeg',
     smallDescription: 'a small description will go here',
     fullDescrition: 'a full description will go here',
   },
   {
     title: 'About Me Game',
-    dateCreated: '04-05-2017',
+    dateCreated: '2017-04-05',
     portfolioPicture: 'img/portfolioPictureOne.jpeg',
     smallDescription: 'a small description will go here',
     fullDescrition: 'a full description will go here',
