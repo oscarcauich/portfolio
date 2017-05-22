0e382eaaafa9c7f530b33c24cb461935a77bef02
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-22 15:45:05 -0700

    fixed some indentation on projectList.js file

--- public/scripts/portfolioscript.js
+++ public/scripts/portfolioscript.js
@@ -4,66 +4,66 @@ var app = app || {};
 
 (function(module){
 // create constructor for projects
-function PortfolioCreate(portolioObjs) {
-  this.title = portolioObjs.title;
-  this.dateCreated = portolioObjs.dateCreated;
-  this.gitHubURL = portolioObjs.gitHubURL;
-  this.portfolioPicture = portolioObjs.portfolioPicture;
-  this. smallDescription = portolioObjs.smallDescription;
-  this.fullDescrition = portolioObjs.fullDescrition;
-}
-
-PortfolioCreate.all = [];
-
-PortfolioCreate.prototype.toHtml = function() {
-  var getTemplate = $('#projects-template').html();
-  var template = Handlebars.compile(getTemplate);
-
-  this.daysAgo = parseInt((new Date() - new Date(this.dateCreated))/60/60/24/1000);
-  this.dateCreated = this.dateCreated ? `published ${this.daysAgo} days ago` : '(draft)';
-
-  return template(this);
-};
-
-PortfolioCreate.loadAll = rows => {
-  rows.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
-
-  PortfolioCreate.all = rows.map((portfolio) => new PortfolioCreate(portfolio));
-};
-
-PortfolioCreate.fetchAll = function() {
-  if (localStorage.projectsData) {
-    let jsonData = JSON.parse(localStorage.projectsData);
-    PortfolioCreate.loadAll(jsonData);
-    projectView.initIndexPage();
-
-  } else {
-    let jsonData = '/data/projects.json';
-    $.getJSON(jsonData)
-        .then(function(projectsData) {
-          localStorage.projectsData = JSON.stringify(projectsData);
-          PortfolioCreate.loadAll(projectsData);
-          projectView.initIndexPage();
-          console.log('workd fool')
-        })
+  function PortfolioCreate(portolioObjs) {
+    this.title = portolioObjs.title;
+    this.dateCreated = portolioObjs.dateCreated;
+    this.gitHubURL = portolioObjs.gitHubURL;
+    this.portfolioPicture = portolioObjs.portfolioPicture;
+    this. smallDescription = portolioObjs.smallDescription;
+    this.fullDescrition = portolioObjs.fullDescrition;
   }
-}
 
-$(document).ready(function() {
-  $('.lnr-cross').hide();
-  $('.lnr-menu').on('click', function(){
-    $('.nav-menu').slideToggle('slow',function(){
-      $('.lnr-menu').hide();
-      $('.lnr-cross').show();
-    });
-  })
-  $('.lnr-cross').on('click', function(){
-    $('.nav-menu').slideToggle('slow',function(){
-      $('.lnr-cross').hide();
-      $('.lnr-menu').show();
+  PortfolioCreate.all = [];
+
+  PortfolioCreate.prototype.toHtml = function() {
+    var getTemplate = $('#projects-template').html();
+    var template = Handlebars.compile(getTemplate);
+
+    this.daysAgo = parseInt((new Date() - new Date(this.dateCreated))/60/60/24/1000);
+    this.dateCreated = this.dateCreated ? `published ${this.daysAgo} days ago` : '(draft)';
+
+    return template(this);
+  };
+
+  PortfolioCreate.loadAll = rows => {
+    rows.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
+
+    PortfolioCreate.all = rows.map((portfolio) => new PortfolioCreate(portfolio));
+  };
+
+  PortfolioCreate.fetchAll = function() {
+    if (localStorage.projectsData) {
+      let jsonData = JSON.parse(localStorage.projectsData);
+      PortfolioCreate.loadAll(jsonData);
+      projectView.initIndexPage();
+
+    } else {
+      let jsonData = '/data/projects.json';
+      $.getJSON(jsonData)
+          .then(function(projectsData) {
+            localStorage.projectsData = JSON.stringify(projectsData);
+            PortfolioCreate.loadAll(projectsData);
+            projectView.initIndexPage();
+            console.log('workd fool')
+          })
+    }
+  }
+
+  $(document).ready(function() {
+    $('.lnr-cross').hide();
+    $('.lnr-menu').on('click', function(){
+      $('.nav-menu').slideToggle('slow',function(){
+        $('.lnr-menu').hide();
+        $('.lnr-cross').show();
+      });
     })
-  })
-});
-// module.articleView = articleView;
-module.PortfolioCreate = PortfolioCreate;
+    $('.lnr-cross').on('click', function(){
+      $('.nav-menu').slideToggle('slow',function(){
+        $('.lnr-cross').hide();
+        $('.lnr-menu').show();
+      })
+    })
+  });
+
+  module.PortfolioCreate = PortfolioCreate;
 })(app)
