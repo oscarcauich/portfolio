a781eeb292de12a3ec31e4e4efad59c07797a8e3
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-23 22:14:25 -0700

    setup the page function for projects link

--- public/scripts/controller/projectController.js
+++ public/scripts/controller/projectController.js
@@ -3,6 +3,12 @@
 var app = app || {};
 
 (function(module) {
+  const projectController = {};
 
-
+  projectController.init = function(){
+    app.PortfolioCreate.fetchAll(projectView.initIndexPage);
+    $('.data-content').hide();
+    $('#projects').fadeIn();
+  }
+  module.projectController = projectController;
 })(app);
